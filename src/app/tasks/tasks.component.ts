db739e7c589fdb8afa2c7080af2fa3f14cc77a3e
Abdul_Malik <malik.info28@gmail.com>
2025-01-03 21:11:09 +0530

    fixed tasks error 'userId === this.userId'

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,17 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
+  imports: [TaskComponent],
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
-  dummyTasks = [
+  tasks = [
     {
       id: 't1',
       userId: 'u1',
@@ -35,10 +35,9 @@ export class TasksComponent {
         'Prepare and describe an issue template which will help with project management',
       dueDate: '2024-06-15',
     },
-  ]
+  ];
 
   get selectedUserTasks() {
-    return this.dummyTasks.filter((task) => task.userId = this.userId);
+    return this.tasks.filter((task) => task.userId === this.userId);
   }
-
 }
